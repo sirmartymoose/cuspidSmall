032715d2c43e21d7e80d4d348897a4f7c5a86044
sirmartymoose <sirmartymoose@gmail.com>
2015-11-03 16:53:41 +0000

    added booking and states

--- server/bookingLogic.js
+++ server/bookingLogic.js
@@ -3,7 +3,20 @@
 Meteor.methods({
     
     markOpportunityAsBooked: function(oId){
+        var outData = opportunities.find({_id: oId}).fetch()
+        //var rawPay = (parseInt(outData['endTime']) - parseInt(outData['startTime'])) * outData['hourlyRate']
+        //rawPay = parseInt(rawPay)
+        //console.log(rawPay)
+        var contractedDate = new Date()
+        
+        
         opportunities.update({_id: oId},{$set: {'booking.isBooked': true}})
+        opportunities.update({_id: oId},{$set: {'booking.assistantId': this.userId}})
+        opportunities.update({_id: oId},{$set: {'state': 'booking'}})
+        opportunities.update({_id: oId},{$set: {'booking.hasOccurred': true}})
+        opportunities.update({_id: oId},{$set: {'booking.contractedDate': contractedDate}})
+        //opportunities.update({_id: oId},{$set: {'booking.agreedAmount': rawPay}})
+        
     }
     
 })
